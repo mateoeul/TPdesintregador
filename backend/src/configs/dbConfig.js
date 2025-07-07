cc00f76a2385faadc05ce0241f4436e10c742c0a
Android Studio <devandroid@ort>
2025-07-07 14:26:28 -0300

    estructura del back

---
+++ backend/src/configs/dbConfig.js
@@ -0,0 +1,11 @@
+import 'dotenv/config'
+
+const DBconfig = {
+    host        :   process.env.DB_HOST     ??  '',
+    database    :   process.env.DB_DATABASE ??  '',
+    user        :   process.env.DB_USER        ??  '',
+    password    :   process.env.DB_PASSWORD    ??  '',
+    port        :   process.env.DB_PORT     ??  '',
+}
+
+export default DBconfig;
