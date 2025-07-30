f66bde0afdf3f9bfa32c3e00648c90c9aad1adfc
mateoeulORT <48044168@est.ort.edu.ar>
2025-07-30 16:31:37 -0300

    event-locations terminado

--- backend/src/repositories/eventLocation-repository.js
+++ backend/src/repositories/eventLocation-repository.js
@@ -21,6 +21,22 @@ export default class EventLocationRepository {
         return returnArray;
     }
 
+    getAllByUserIdAsync = async (userId) => {
+        let returnArray = null;
+        const client = new Client(DBconfig)
+
+        try {
+            await client.connect();
+            const result = await client.query("SELECT * FROM event_locations WHERE id_creator_user = $1", [userId]);
+            await client.end();
+            returnArray = result.rows;
+        } catch (error) {
+            console.log("Error en getAllByUserIdAsync:", error);
+        }
+
+        return returnArray;
+    }
+
     getByIdAsync = async(id) =>{
         const client = new Client(DBconfig);
         try {
@@ -30,6 +46,35 @@ export default class EventLocationRepository {
             return result.rows[0]; // Devuelve solo una ubicación
         } catch (error) {
             console.log("Error en getByIdAsync:", error);
+            return null;
+        }
+    }
+
+    getByIdAndUserIdAsync = async(id, userId) =>{
+        const client = new Client(DBconfig);
+        try {
+            await client.connect();
+            const result = await client.query("SELECT * FROM event_locations WHERE id = $1 AND id_creator_user = $2", [id, userId]);
+            await client.end();
+            return result.rows[0];
+        } catch (error) {
+            console.log("Error en getByIdAndUserIdAsync:", error);
+            return null;
+        }
+    }
+
+    createAsync = async(eventLocation) => {
+        const client = new Client(DBconfig);
+        try {
+            await client.connect();
+            const result = await client.query(
+                "INSERT INTO event_locations (id_location, name, full_address, max_capacity, latitude, longitude, id_creator_user) VALUES ($1, $2, $3, $4, $5, $6, $7) RETURNING *",
+                [eventLocation.id_location, eventLocation.name, eventLocation.full_address, eventLocation.max_capacity, eventLocation.latitude, eventLocation.longitude, eventLocation.id_creator_user]
+            );
+            await client.end();
+            return result.rows[0];
+        } catch (error) {
+            console.log("Error en createAsync:", error);
             return null;
         }
     }
